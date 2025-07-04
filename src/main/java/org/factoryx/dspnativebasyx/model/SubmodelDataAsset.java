3c72d8e590af1336fd29738d88b6a3c2ccb76d91
Ernst-Christoph Schrewe <ernst-christoph.schrewe@isst.fraunhofer.de>
2025-07-04 10:20:53 +0200

    feat: cleanup

--- src/main/java/org/factoryx/dspnativebasyx/model/SubmodelDataAsset.java
+++ src/main/java/org/factoryx/dspnativebasyx/model/SubmodelDataAsset.java
@@ -1,14 +1,14 @@
 package org.factoryx.dspnativebasyx.model;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.eclipse.digitaltwin.aas4j.v3.model.Referable;
 import org.eclipse.digitaltwin.aas4j.v3.model.Submodel;
 import org.factoryx.library.connector.embedded.provider.interfaces.DataAsset;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 import static org.factoryx.dspnativebasyx.model.AasDataAsset.MAPPER;
 
@@ -43,8 +43,10 @@ public class SubmodelDataAsset implements DataAsset {
     @Override
     public byte[] getDtoRepresentation() {
         try {
-            return MAPPER.writeValueAsBytes(submodel);
-        } catch (JsonProcessingException e) {
+            ObjectNode node = MAPPER.convertValue(submodel, ObjectNode.class);
+            node.put("modelType", "Submodel");
+            return node.toString().getBytes(StandardCharsets.UTF_8);
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
