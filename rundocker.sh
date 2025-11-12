8c3c44bd0e5bdcd1cbc44ba2454f487f6afc2ba5
Ernst-Christoph Schrewe <132669361+eschrewe@users.noreply.github.com>
2025-11-12 12:57:43 +0100

    fix: fix dockerfile
    * fix: dockerfile
    
    * fix: missing change in bruno collection
    
    ---------
    
    Co-authored-by: Haydar Qarawlus <haydar.qarawlus@isst.fraunhofer.de>

--- rundocker.sh
+++ rundocker.sh
@@ -9,14 +9,5 @@ popd >/dev/null
 echo "Removing previous containers"
 docker compose down -v
 
-echo "Building .jar"
-./mvnw clean package -DskipTests
-
-echo "Building docker image for DSP-Native-BaSyx"
-docker build \
-  --secret id=github-token,env=GITHUB_TOKEN \
-  --secret id=github-actor,env=GITHUB_ACTOR \
-  -t dsp-native-basyx:latest .
-
 echo "Starting test environment"
 docker compose up --build
